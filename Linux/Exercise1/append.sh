6851c2548624a5bea83c5fdbec34f29d9096febb
Paresh Gupta <paresh@vinsol.com>
2014-03-20 00:25:52 +0530

    changes done

--- Linux/Exercise1/append.sh
+++ Linux/Exercise1/append.sh
@@ -3,5 +3,5 @@
 
 script_dir=$(dirname $0)
 sed  s/\ www\./http:\\/\\/www\./g ./$script_dir/company_name > ./$script_dir/company_name2
-cat company_name2
+cat ./$script_dir/company_name2
 
--- Linux/Exercise2/change-version.sh
+++ Linux/Exercise2/change-version.sh
@@ -2,6 +2,8 @@
 #changing version:1.0 to version:2.0 in a 
 
 script_dir=$(dirname $0)
-cp -r $script_dir/project/abc/1.0/* $script_dir/project/abc/2.0
-grep -lr "Version:1.0" $script_dir/project/abc/2.0/* | xargs sed -i  "s/Version:1.0/Version:2.0/g"
+path=$script_dir/project/abc/2.0
+mkdir $path
+cp -ar  $script_dir/project/abc/1.0/* $path
+grep -lr "Version:1.0" $path | xargs sed -i  "s/Version:1.0/Version:2.0/g"
 

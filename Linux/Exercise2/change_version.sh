e0bf62762d182078a2a7fc98426376486a06d2ba
Paresh Gupta <paresh@vinsol.com>
2014-03-27 14:23:09 +0530

    variable name change

--- Linux/Exercise2/change_version.sh
+++ Linux/Exercise2/change_version.sh
@@ -2,7 +2,7 @@
 #changing version:1.0 to version:2.0 in a 
 
 script_directory=$(dirname $0)
-path=$script_directory/project/abc/2.0
-mkdir $path
-cp -r  $script_directory/project/abc/1.0/* $path
-sed -i '0,/Version:1.0/s/Version:1.0/Version:2.0/' $path/code/*
+new_folder_path=$script_directory/project/abc/2.0
+mkdir $new_folder_path
+cp -r  $script_directory/project/abc/1.0/* $new_folder_path
+sed -i '0,/Version:1.0/s/Version:1.0/Version:2.0/' $new_folder_path/code/*

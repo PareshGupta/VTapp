e27b11fff5e56ab63d92f778d279d39e531c18b8
Paresh Gupta <paresh@vinsol.com>
2014-04-17 12:49:03 +0530

    exercise 3 added

---
+++ JavaScript/Exercise 3/javascript3.js
@@ -0,0 +1,25 @@
+var x = document.getElementsByTagName("input");
+// function for maximum limit
+function myfunction() {
+  var total = 0;
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].checked) {
+      total = total + 1;
+    }
+    if (total > 3) {
+      alert("You Can Maximum Select 3 Checkboxes");
+      x[i].checked = false;
+      return false;
+    }
+  }
+}
+
+// function for select none
+function NoneSelected(choice) {
+  var y = x.length - 1;
+  i = 0;
+  while (i < y) {
+    x[i].checked = choice;
+    i++;
+  }
+}

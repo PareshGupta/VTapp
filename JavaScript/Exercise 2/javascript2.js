2e116a002ef5aa9045d9b68f0e4471fe13a7e333
Paresh Gupta <paresh@vinsol.com>
2014-04-22 11:37:37 +0530

    done using oops concept

--- JavaScript/Exercise 2/javascript2.js
+++ JavaScript/Exercise 2/javascript2.js
@@ -1,9 +1,42 @@
-function CheckAll(source){
-  var x = document.getElementsByTagName("input");
-  var y = x.length;
-  i=0;
-  while(i<y){
-    x[i].checked = source;
-    i++;
+// creating a class
+function Form(form_id){
+  this.form_id = form_id;
+
+//creating method to get checkbox elements 
+  this.getAllCheckboxes = function(){
+    var form = document.getElementById(this.form_id);   
+    var checkbox = form.getElementsByClassName("check");
+    return checkbox;
+  }
+
+//method to check all checkboxes  
+  this.checkAll = function(){
+    this.checkAllOrNone(true);  
+  }
+
+// method to uncheck all checkboxes
+  this.uncheckAll = function(){
+    this.checkAllOrNone(false); 
+  }
+
+// method for iterate to all checkboxes
+  this.checkAllOrNone = function(choice){
+    var all_checkboxes = this.getAllCheckboxes();
+    for(var i = 0; i < all_checkboxes.length; i++){
+      all_checkboxes[i].checked = choice;
+    }
+  }
+
+// method for onclick event 
+  this.events = function(){
+    var form = document.getElementById(this.form_id);
+    var checkall_link = form.getElementsByClassName("checkall")[0];
+    var checknone_link = form.getElementsByClassName("checknone")[0];
+    var value = this;
+    checkall_link.onclick = function(){ value.checkAll(); }
+    checknone_link.onclick = function(){ value.uncheckAll(); }
   }
 }
+
+var form1 = new Form("form1");
+form1.events();
--- JavaScript/Exercise 3/javascript3.js
+++ JavaScript/Exercise 3/javascript3.js
@@ -1,28 +1,59 @@
-var x = document.getElementsByTagName("input");
-// function for maximum limit
-function myfunction() {
-  var none_checkbox = document.getElementById("none");
-  var total = 0;
-  for (var i = 0; i < x.length; i++) {
-    if (x[i].checked) {
-      total = total + 1;
-      none_checkbox.checked=false;
+// creating a class
+var Form = function(form_id){
+  this.form_id = form_id;
 
+  // method to get all checkboxes
+  this.getCheckBox = function(){
+    var allcheckbox = document.getElementsByName("days");
+    return allcheckbox;
+  }
+  
+  // method for uncheck all checkboxes
+  this.checkNone = function(){
+    var all_checkboxes = this.getCheckBox();
+    for(var i = 0; i < all_checkboxes.length; i++){
+      all_checkboxes[i].checked = false;
     }
-    if (total > 3) {
-      alert("You Can Maximum Select 3 Checkboxes");
-      x[i].checked = false;
-      return false;
-    }
+  }
+
+  this.uncheckAllCheckBoxes = function(){
+    this.checkNone(); 
+  }
+
+  // method to select maximum of three checkboxes
+  this.checkMaximumThreeCheckBoxes = function(){
+    var checkboxes = this.getCheckBox();
+    var none_checkbox = document.getElementById("none");
+    var total = 0;
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked == true) {
+        none_checkbox.checked = false;
+        total = total + 1;
+      } 
+      
+      if (total > 3) {
+        checkboxes[i].checked = false;
+        alert("You Can Maximum Select 3 Checkboxes");
+        return false;
+      }
+    }  
+  }
+
+  this.checkThreeCheckBoxes = function() {
+    this.checkMaximumThreeCheckBoxes();
+  }
+  
+  // method of creating event
+  this.events = function(){
+    var all_checkboxes = this.getCheckBox();
+    var b = document.getElementById("none"); 
+    
+    var value = this;
+
+    // all_checkboxes.onclick = function() {value.checkThreeCheckBoxes(); }
+    b.onclick = function() {value.checkNone(); }
   }
 }
 
-// function for select none
-function NoneSelected(choice) {
-  var y = x.length - 1;
-  i = 0;
-  while (i < y) {
-    x[i].checked = choice;
-    i++;
-  }
-}
+var form1 = new Form("form1");
+form1.events();

680501b9d53fe9313ef8a59cc52760769df4bf62
Paresh Gupta <paresh@vinsol.com>
2014-05-03 13:52:43 +0530

    changes done

--- JavaScript/Exercise 4/javascript4.js
+++ JavaScript/Exercise 4/javascript4.js
@@ -1,5 +1,5 @@
 // creating a class Row
-var Row = function(index){  
+var Row = function(index) {  
   this.row = document.getElementsByTagName("tr")[index];
   this.cell = this.row.getElementsByTagName("td");
   this.saveEvent();
@@ -9,61 +9,58 @@ var Row = function(index){
 Row.prototype.saveEvent = function() {
   var save = this.row.getElementsByClassName("save")[0];
   var that = this;
-  save.onclick = function(){that.inputValues();}
-}
-
-// create anchor tag 
-Row.prototype.createAnchorTag1 = function() {
-  var anchor_tag1 = document.createElement("a");
-  anchor_tag1.innerHTML ="Edit";
-  anchor_tag1.setAttribute("class",'edit');
-  anchor_tag1.setAttribute("href","#");
-  return anchor_tag1;
-}
-
-//  create anchor tag
-Row.prototype.createAnchorTag2 = function() {
-  var anchor_tag2= document.createElement("a");
-  anchor_tag2.innerHTML ="Delete";
-  anchor_tag2.setAttribute("class",'del');
-  anchor_tag2.setAttribute("href","#");
-  return anchor_tag2;
+  save.onclick = function(){ that.inputValues(); };
 }
 
 // method to add values to the row
 Row.prototype.inputValues = function() {
   var input = this.row.getElementsByTagName("input");
-  var anchor1 = this.createAnchorTag1();
-  var anchor2 = this.createAnchorTag2();
+  var edit = this.createAnchorTag("Edit");
+  var del = this.createAnchorTag("Delete");
   this.cell[0].innerHTML = input[0].value;
   this.cell[1].innerHTML = input[0].value; 
-  this.cell[2].replaceChild(anchor1,this.cell[2].childNodes[0]);
-  this.cell[2].appendChild(anchor2);  
-  this.editEvent();
-  this.deleteEvent();
+  this.cell[2].replaceChild(edit, this.cell[2].childNodes[0]);
+  this.cell[2].appendChild(del);  
+  this.editAndDeleteEvent();
 }
 
-// edit event 
-Row.prototype.editEvent = function() {
-  var edit = this.row.getElementsByClassName("edit")[0];
+// edit and delete event 
+Row.prototype.editAndDeleteEvent = function() {
+  var edit = this.row.getElementsByClassName("Edit")[0];
+  var del = this.row.getElementsByClassName("Delete")[0];
   var that = this;
-  edit.onclick = function(){that.editRowValues();};
+  edit.onclick = function(){ that.editRowValues(); };
+  del.onclick = function(){ that.row.remove(); };
 }
 
 // create Save button
 Row.prototype.createButton = function() {
   var button_tag = document.createElement("button");
-  button_tag.setAttribute("class","save");
+  button_tag.setAttribute("class", "save");
   button_tag.innerHTML = "Save";
   return button_tag;
 }
 
+// create Anchor tag 
+Row.prototype.createAnchorTag = function(choice) {
+  var anchor_tag = document.createElement("a");
+  anchor_tag.innerHTML = choice;
+  anchor_tag.setAttribute("class", choice);
+  anchor_tag.setAttribute("href", "#");
+  return anchor_tag;
+}
+
+// create Input tag
+Row.prototype.createInputTag = function(choice) {
+  var input = document.createElement("input");
+  input.setAttribute("value", choice.innerText);
+  return input;
+}
+
 // method to edit the row
 Row.prototype.editRowValues = function() {
-  var input1 = document.createElement("input");
-  input1.setAttribute("value",this.cell[0].innerText);
-  var input2 = document.createElement("input");
-  input2.setAttribute("value",this.cell[1].innerText);
+  var input1 = this.createInputTag(this.cell[0]);
+  var input2 = this.createInputTag(this.cell[1]);
   var save = this.createButton();
   this.cell[0].replaceChild(input1,this.cell[0].childNodes[0]);
   this.cell[1].replaceChild(input2,this.cell[1].childNodes[0]);
@@ -72,44 +69,36 @@ Row.prototype.editRowValues = function() {
   this.saveEvent();
 }
 
-// delete event
-Row.prototype.deleteEvent = function() {
-  var del = this.row.getElementsByClassName("del")[0];
-  var that = this;
-  del.onclick = function(){that.deleteRow();};
-}
-
-// method to delete the row 
-Row.prototype.deleteRow = function(){
-  this.row.remove();
-}
-
 // creating a new class
 var Table = function(id) {
+  var that = this;
   this.table = document.getElementById(id);
-  this.addInstance();
+  this.add = document.getElementById("button"); 
+  this.add.onclick = function(){ that.createRow(); };
   this.createRow();
 }
 
-// add event
-Table.prototype.addInstance = function() {
-  var add = document.getElementById("button");
-  var that = this;
-  add.onclick = function(){that.createRow();};
+// creating new tags
+Table.prototype.createTags = function(choice) {
+  var inputTag = document.createElement(choice);
+  return inputTag; 
+}
+
+// creating new button tags
+Table.prototype.createButtonTags = function() {
+  var buttonTag = document.createElement("button");
+  buttonTag.setAttribute("class", "save");
+  buttonTag.innerHTML = "Save";
+  return buttonTag;
 }
 
 // creating rows and instances of Row class
 Table.prototype.createRow = function() {
   var row = this.table.insertRow(-1);
-  var row_attribute = row.setAttribute("class","tablerow");
-  var input_tag1 = document.createElement("input");
-  var input_tag2 = document.createElement("input");
-  var button_tag = document.createElement("button");
-  button_tag.setAttribute("class","save");
-  button_tag.innerHTML = "Save";
-  row.insertCell(0).appendChild(input_tag1);
-  row.insertCell(1).appendChild(input_tag2);
-  row.insertCell(2).appendChild(button_tag);
+  row.setAttribute("class", "tablerow");
+  row.insertCell(0).appendChild(this.createTags("input"));
+  row.insertCell(1).appendChild(this.createTags("input"));
+  row.insertCell(2).appendChild(this.createButtonTags());
   new Row(document.getElementsByTagName("tr").length - 1);
 }
 

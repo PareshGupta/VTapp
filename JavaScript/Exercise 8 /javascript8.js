4287dc1b610a916994f621741c2719ce1876eaca
Paresh Gupta <paresh@vinsol.com>
2014-05-03 13:55:03 +0530

    regex improved and validator object added

--- JavaScript/Exercise 8 /javascript8.js
+++ JavaScript/Exercise 8 /javascript8.js
@@ -1,19 +1,21 @@
-var Window = function() {  
+// class Prompt for new url
+var Prompt = function() {  
+  this.url = prompt("Enter the url: ", "http://www.google.com");
+  urlValidator.validateOrOpenUrl(this.url);
+}
 
-  this.enterValidURL = function() {
-    var url = " ";
-    var pattern = /(http|https):\/\/(w{3})\.([\w]+)\.\w{2,6}/;
-    do {
-      url = prompt("Enter the url: " , "http://www.google.com");
-      if (!pattern.test(url)) {
-        alert("Invalid url entered!! Kindly enter it again !!");
-      }
-      else {
-        window.open(url , "_blank" , "width=400 , height=450 , menubar=0 , scrollbars=0 , status=0");
-      }
-    } while (!pattern.test(url));
+// object for validating url 
+var urlValidator = {
+  pattern : /(http(s?)\:\/\/){1}(w{3}\.)?([\w]+)\.\w{2,6}/,
+  
+  validateOrOpenUrl : function(choice) {  
+    if (!this.pattern.test(choice)) {
+      alert("Invalid url entered!! Kindly enter it again !!");
+      return new Prompt();
+    } else {
+      window.open(choice, "_blank", "width=400, height=450, menubar=0, scrollbars=0, status=0");
+    } 
   }
 }
 
-var urlPrompt = new Window();
-urlPrompt.enterValidURL();
+window.onload = function() { new Prompt(); };

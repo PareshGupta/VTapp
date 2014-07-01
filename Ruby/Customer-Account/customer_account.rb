578b548cbfaa647062242155910f7d0d8a09cd40
paresh <paresh@vinsol.com>
2014-07-02 01:51:36 +0530

    deposit + withdraw method add

--- Ruby/Customer-Account/customer_account.rb
+++ Ruby/Customer-Account/customer_account.rb
@@ -1,20 +1,35 @@
 class Customer
-  attr_accessor :name
-  attr_reader :account_no
+	attr_reader :name, :balance, :account_number
+ 	@@account_no = 0
 
-  def initialize(name, account_no, balance)
+  def initialize(name)
     @name = name
-    @account_no = account_no
-    @balance = balance
+    @balance = 1000
+    @@account_no += 1
+  	@account_number = @@account_no
   end
 
-  def account_no=(account_no)
-    @account_no = account_no
+  def deposit(amount)
+    @balance += amount
+    puts "Hello Mr.#{@name} \nRs#{amount}/- has been deposited in your account \nYour balance is Rs#{balance}/-"
+  end
+  
+  def withdraw(amount)
+    if amount >= @balance
+      puts "Transaction cannot be done \nPlease check the balance"
+    else
+      @balance -= amount
+      puts "Hello Mr.#{@name} \nRs#{amount}/- has been withdrawn from your account \nYour balance is #{balance}"
+    end
   end
 end
 
-customer = Customer.new("Paresh", 11000999934618, 1000)
-puts customer.inspect
-puts customer.name
-puts customer.account_no
+customer = Customer.new("Paresh")
+# puts customer.inspect
+customer.deposit(1000)
+customer.withdraw(700)
 
+customer2 = Customer.new("Bharat")
+# puts customer2.inspect
+customer2.deposit(3000)
+customer2.withdraw(5700)

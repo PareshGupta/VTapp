1980f412df81376304cbb7b9f10e9fe007dbad83
Paresh Gupta <paresh@vinsol.com>
2014-07-01 17:33:31 +0530

    occurence of each alphabets exercise add

---
+++ Ruby/Occurence/bin/main.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/occurence.rb'
+count = Count.new
+count.alphabets
---
+++ Ruby/Occurence/lib/occurence.rb
@@ -0,0 +1,14 @@
+class Count
+  def alphabets
+    print "Enter a string : "
+    string = gets()
+    print "Occurence of each alphabet in a string is : "
+    alphabets_count = Hash.new
+    string.scan(/[a-z]|[A-Z]/).each{|i|
+      alphabets_count[i] = string.count(i)
+    }
+    puts alphabets_count
+  end
+end
+
+

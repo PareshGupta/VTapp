f9636c3c717cb9fdfe1f961fc6d6cfeef3421ab2
paresh <paresh@vinsol.com>
2014-07-04 01:45:34 +0530

    spacing issue solved

--- Ruby/Customer-Account/lib/customer.rb
+++ Ruby/Customer-Account/lib/customer.rb
@@ -1,11 +1,11 @@
 class Customer
-	attr_accessor :name, :balance, :account_number
- 	@@account_no = 99111003461124
+  attr_accessor :name, :balance, :account_number
+  @@account_no = 99111003461124
 
   def initialize(name)
     @name = name
     @balance = 1000
-  	@account_number = @@account_no
+    @account_number = @@account_no
     @@account_no += 1
   end
 
@@ -13,7 +13,7 @@ class Customer
     @balance += amount
     puts "Hello Mr.#{@name} \nRs#{amount}/- has been deposited in your account \nYour balance is Rs#{balance}/-"
   end
-  
+
   def withdraw(amount)
     if amount >= @balance
       puts "Transaction cannot be done \nPlease check the balance"

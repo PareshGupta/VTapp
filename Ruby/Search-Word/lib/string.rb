8d400156eafa756bf07b85cb146482b464376dd7
Paresh Gupta <paresh@vinsol.com>
2014-07-05 18:36:16 +0530

    regex changed

--- Ruby/Search-Word/lib/string.rb
+++ Ruby/Search-Word/lib/string.rb
@@ -1,15 +1,9 @@
 class String
 
   def search_pattern(word)
-    occurence_count = 0
-    regex_to_search = Regexp.new(word, true)
-    searched_string = gsub(regex_to_search) do |match|
-      occurence_count += 1
-      "(#{word})"
-    end
+    occurence_count = scan(/#{word}/i).size
+    searched_string = gsub(/(?<word>#{word})/i, '(\k<word>)')
     "#{searched_string} \nTotal Occurence is #{occurence_count}"
   end
-
-  def check_character_case
 
 end

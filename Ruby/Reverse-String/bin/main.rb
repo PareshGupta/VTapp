92764152491957baa89a9970567e3aa41a29dfbd
Paresh Gupta <paresh@vinsol.com>
2014-07-08 12:32:09 +0530

    add spaces around #{ }

--- Ruby/Reverse-String/bin/main.rb
+++ Ruby/Reverse-String/bin/main.rb
@@ -2,4 +2,4 @@ require_relative '../lib/string.rb'
 
 puts 'Enter a string :'
 string = gets.chomp
-puts "The reverse of the sentence is : #{string.arrange_words_in_reverse_order}"
+puts "The reverse of the sentence is : #{ string.arrange_words_in_reverse_order }"

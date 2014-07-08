82773a587889dc306dd4a70d5223eb73a2bfa65f
Paresh Gupta <paresh@vinsol.com>
2014-07-08 20:03:54 +0530

    use prime class method

--- Ruby/Prime-Number/bin/main.rb
+++ Ruby/Prime-Number/bin/main.rb
@@ -2,4 +2,4 @@ require_relative '../lib/fixnum.rb'
 
 print 'Enter a number upto which you want to generate prime series : '
 number = gets.chomp.to_i
-print "Prime series upto #{number} is \n#{number.generate_primes}"
+print number.generate_prime_upto

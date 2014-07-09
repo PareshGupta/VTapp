cb8240cec362633b2cd3ce50b373d06619194cb6
Paresh Gupta <paresh@vinsol.com>
2014-07-09 14:12:43 +0530

    add to_s method

--- Ruby/Vehicle/bin/main.rb
+++ Ruby/Vehicle/bin/main.rb
@@ -2,6 +2,6 @@ require_relative '../lib/vehicle.rb'
 require_relative '../lib/bike.rb'
 
 bike = Bike.new('Xtreme', 65000, 'Raju')
-puts bike.inspect
+puts bike
 bike.price = 60000
-puts bike.inspect
+puts bike
--- Ruby/Vehicle/lib/bike.rb
+++ Ruby/Vehicle/lib/bike.rb
@@ -5,4 +5,8 @@ class Bike < Vehicle
     @dealer = dealer
   end
 
+  def to_s
+    "Dealer #{ @dealer} is selling the bike #{ @name } at price Rs #{ @price }/-"
+  end
+
 end

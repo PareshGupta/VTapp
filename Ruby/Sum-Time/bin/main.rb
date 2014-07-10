d6ca5ed89260c7827a696acf9797d069ff12ea81
Paresh Gupta <paresh@vinsol.com>
2014-07-10 20:05:38 +0530

    add new method

--- Ruby/Sum-Time/bin/main.rb
+++ Ruby/Sum-Time/bin/main.rb
@@ -1,19 +1,9 @@
 require_relative '../lib/time.rb'
 
-time = Time.new
 puts 'Enter the first time'
 time1 = gets.chomp
 puts 'Enter the second time'
 time2 = gets.chomp
 
-if time.validate(time1) && time.validate(time2)
-  time1 = DateTime.parse(time1)
-  time2 = DateTime.parse(time2)
-  time_array = Array.new
-  time_array.push(time1, time2)
-  time.sum_total_each_unit(time_array)
-  time.calculate_total
-  puts time
-else
-  print 'Invalid Time'
-end
+time = Time.new
+puts time.parse_and_push_to_array(time1, time2)
--- Ruby/Sum-Time/lib/time.rb
+++ Ruby/Sum-Time/lib/time.rb
@@ -7,6 +7,19 @@ class Time
     CHECK_TIME_REGEX === time
   end
 
+  def parse_and_push_to_array(time1, time2)
+    if validate(time1) && validate(time2)
+      time1 = DateTime.parse(time1)
+      time2 = DateTime.parse(time2)
+      time_array = Array.new
+      time_array.push(time1, time2)
+      sum_total_each_unit(time_array)
+      calculate_total
+    else
+      'Invalid Time'
+    end
+  end
+
   def sum_total_each_unit(time)
     @total_seconds, @total_minutes, @total_hours = 0, 0, 0
     for i in 0...time.length do
@@ -22,9 +35,6 @@ class Time
     extra_hours, @minutes = @total_minutes.divmod(60)
     @total_hours += extra_hours
     @days, @hours = @total_hours.divmod(24)
-  end
-
-  def to_s
     "The total time is #{ @days } day and #{ @hours }:#{ @minutes }:#{ @seconds }"
   end
 

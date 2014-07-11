0d7fcefabe7325418be1c3dc53b5b82d602a7472
Paresh Gupta <paresh@vinsol.com>
2014-07-11 15:49:19 +0530

    solve spacing issues

--- Ruby/Search-Word/lib/string.rb
+++ Ruby/Search-Word/lib/string.rb
@@ -1,11 +1,15 @@
 class String
-  
-  # Regex Description :- Here, (?<word>) is like a variable 'word' which stores the search word in a string
-  # \k<word> is a way to access the search word.
+
+=begin
+  Regex Description :- Here, (?<word>) is like a variable 'word',
+  which stores the search word in a string.
+  \k<word> is a way to access the search word.
+=end
+
   def search_and_highlight_pattern(word)
-    occurence_count = scan(/#{word}/i).size
-    searched_string = gsub(/(?<word>#{word})/i, '(\k<word>)')
-    "#{searched_string} \nTotal Occurence is #{occurence_count}"
+    occurence_count = scan(/#{ word }/i).size
+    searched_string = gsub(/(?<word>#{ word })/i, '(\k<word>)')
+    "#{ searched_string } \nTotal Occurence is #{ occurence_count }"
   end
 
 end

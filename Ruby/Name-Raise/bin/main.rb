826fbd2deb2d10c7dbb942f6f061785a1e8be3dd
Paresh Gupta <paresh@vinsol.com>
2014-07-11 16:41:37 +0530

    add exception for nil value + change exception messages

--- Ruby/Name-Raise/bin/main.rb
+++ Ruby/Name-Raise/bin/main.rb
@@ -1,9 +1,4 @@
 require_relative '../lib/name.rb'
 
-puts 'Enter the first name :'
-firstname = gets.chomp
-puts 'Enter the last name :'
-lastname = gets.chomp
-
-name = Name.new(firstname, lastname)
+name = Name.new("Paresh", "gupta")
 puts name.validate!
--- Ruby/Name-Raise/lib/name.rb
+++ Ruby/Name-Raise/lib/name.rb
@@ -9,10 +9,12 @@ class Name
   end
 
   def validate!
-    if(@firstname.length == 0 || @lastname.length == 0)
-      raise NameError, 'Invalid Name'
-    elsif(@firstname[0].match(CHECK_NAME_REGEX))
-      raise NameError, 'Invalid Name'
+    if @firstname.nil? || @firstname.length == 0
+      raise NameError, 'Firstname is Invalid'
+    elsif @firstname[0].match(CHECK_NAME_REGEX)
+      raise NameError, 'Firstname First-letter is not Capital'
+    elsif @lastname.nil? || @lastname.length == 0
+      raise NameError, 'Lastname is Invalid'
     end
     "#{ @firstname } #{ @lastname }"
   end

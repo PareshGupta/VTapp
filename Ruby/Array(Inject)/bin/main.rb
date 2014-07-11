51d006591a724f5aeb393b9b897e8b62ecd0f1d8
Paresh Gupta <paresh@vinsol.com>
2014-07-11 14:13:40 +0530

    replace string to symbols + change method names

--- Ruby/Array(Inject)/bin/main.rb
+++ Ruby/Array(Inject)/bin/main.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/array.rb'
 
-puts [123, 'efg', 'abcd', 'hell', 1408, 25, 'Go'].to_hash
+puts [123, 'efg', 'abcd', 'hell', 1408, 25, 'Go'].group_elements_by_even_and_odd
--- Ruby/Array(Inject)/lib/array.rb
+++ Ruby/Array(Inject)/lib/array.rb
@@ -1,6 +1,6 @@
 class Array
 
-  def get_elements
+  def group_elements_by_length
     @temp_object = Hash.new { |hash, key| hash[key] = Array.new }
     for item in self
       @temp_object[item.to_s.length].push(item)
@@ -8,13 +8,13 @@ class Array
     @temp_object
   end
 
-  def to_hash
+  def group_elements_by_even_and_odd
     hash_object = Hash.new { |hash, key| hash[key] = Array.new }
-    get_elements.inject({}) do |hash, (key, value)|
+    group_elements_by_length.inject({}) do |hash, (key, value)|
       if(key.odd?)
-        hash_object['odd'].push(value)
+        hash_object[:odd].push(value)
       else
-        hash_object['even'].push(value)
+        hash_object[:even].push(value)
       end
     end
     hash_object

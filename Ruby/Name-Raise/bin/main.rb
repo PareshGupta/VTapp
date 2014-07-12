2243e4bed975fb26511199061d587fa2fe4da0e9
Paresh Gupta <paresh@vinsol.com>
2014-07-12 15:38:24 +0530

    separate method to check capital

--- Ruby/Name-Raise/bin/main.rb
+++ Ruby/Name-Raise/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/name.rb'
 
-name = Name.new("Paresh", "gupta")
-puts name.validate!
+name = Name.new('Paresh', 'gupta')
+puts name
--- Ruby/Name-Raise/lib/name.rb
+++ Ruby/Name-Raise/lib/name.rb
@@ -4,18 +4,27 @@ class Name
   CHECK_NAME_REGEX = /[a-z]|[\d]/
 
   def initialize(firstname, lastname)
-    @firstname = firstname
-    @lastname = lastname
+    @firstname = validate_length!(firstname) && is_capital!(firstname)
+    @lastname = validate_length!(lastname)
   end
 
-  def validate!
-    if @firstname.nil? || @firstname.length == 0
-      raise NameError, 'Firstname is Invalid'
-    elsif @firstname[0].match(CHECK_NAME_REGEX)
-      raise NameError, 'Firstname First-letter is not Capital'
-    elsif @lastname.nil? || @lastname.length == 0
-      raise NameError, 'Lastname is Invalid'
+  def validate_length!(name)
+    if name.nil? || name.empty?
+      raise NameError, 'Name is Empty'
+    else
+      name
     end
+  end
+
+  def is_capital!(name)
+    if name[0].match(CHECK_NAME_REGEX)
+      raise NameError, 'First-letter of the name is not Capital'
+    else
+      name
+    end
+  end
+
+  def to_s
     "#{ @firstname } #{ @lastname }"
   end
 

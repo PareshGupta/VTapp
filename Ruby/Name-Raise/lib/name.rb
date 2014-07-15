e7be9a6c1312adf7778fef96f5e60917947dbfb6
Paresh Gupta <paresh@vinsol.com>
2014-07-15 17:51:04 +0530

    change regex name + method name

--- Ruby/Name-Raise/lib/name.rb
+++ Ruby/Name-Raise/lib/name.rb
@@ -1,28 +1,30 @@
-require_relative '../lib/name_error.rb'
+require_relative '../lib/invalid_name_error.rb'
 
 class Name
-  CHECK_NAME_REGEX = /[a-z]|[\d]/
+  CHECK_LOWERCASE_ALPHABETS_OR_DIGITS_REGEX = /[a-z]|[\d]/
 
   def initialize(firstname, lastname)
-    @firstname = validate_length!(firstname) && is_capital!(firstname)
+    @firstname = validate_length!(firstname) && check_first_letter_capital!(firstname)
     @lastname = validate_length!(lastname)
   end
-
-  def validate_length!(name)
-    if name.nil? || name.empty?
-      raise NameError, 'Name is Empty'
-    else
-      name
+  
+  private
+    def validate_length!(name)
+      if name.nil? || name.empty?
+        raise NameError, "Name is Empty"
+      else
+        name
+      end
     end
-  end
-
-  def is_capital!(name)
-    if name[0].match(CHECK_NAME_REGEX)
-      raise NameError, 'First-letter of the name is not Capital'
-    else
-      name
+  
+  private
+    def check_first_letter_capital!(name)
+      if name[0].match(CHECK_LOWERCASE_ALPHABETS_OR_DIGITS_REGEX)
+        raise NameError, 'First-letter of the name is not Capital'
+      else
+        name
+      end
     end
-  end
 
   def to_s
     "#{ @firstname } #{ @lastname }"

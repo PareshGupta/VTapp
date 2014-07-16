83b33ae98b9488a36153f87aa26c61c41851adbe
Paresh Gupta <paresh@vinsol.com>
2014-07-16 16:26:40 +0530

    change instance methods to class methods

--- Ruby/Sum-Time/bin/main.rb
+++ Ruby/Sum-Time/bin/main.rb
@@ -1,8 +1,3 @@
 require_relative '../lib/time.rb'
 
-puts 'Enter the first time'
-time1 = gets.chomp
-puts 'Enter the second time'
-time2 = gets.chomp
-
-puts Time.new.sum(time1, time2)
+puts Time.sum('19:23:34', '10:32:45')
--- Ruby/Sum-Time/lib/time.rb
+++ Ruby/Sum-Time/lib/time.rb
@@ -2,32 +2,33 @@ require 'date'
 
 class Time
   CHECK_TIME_REGEX = /(^([0-1]?\d|2[0-3]?)\:([0-5]?\d)\:([0-5]?\d)$)/
-  attr_reader :total_hours, :total_minutes, :total_seconds
 
-  def validate(time)
+  def valid?(time)
     CHECK_TIME_REGEX === time
   end
 
   def parse_and_calculate_total_units(time1, time2)
-    if validate(time1) && validate(time2)
+    if valid?(time1) && valid?(time2)
       time1 = DateTime.parse(time1)
       time2 = DateTime.parse(time2)
-      @total_hours = time1.hour + time2.hour
-      @total_minutes = time1.minute + time2.minute
-      @total_seconds = time1.second + time2.second 
-    else
-      'Invalid Time'
+      @@total_hours = time1.hour + time2.hour
+      @@total_minutes = time1.minute + time2.minute
+      @@total_seconds = time1.second + time2.second
     end
   end
 
-  def sum(time1, time2)
-    parse_and_calculate_total_units(time1, time2)
-    extra_minutes, seconds = total_seconds.divmod(60)
-    @total_minutes += extra_minutes
-    extra_hours, minutes = total_minutes.divmod(60)
-    @total_hours += extra_hours
-    days, hours = total_hours.divmod(24)
-    "The total time is #{ days } day and #{ hours }:#{ minutes }:#{ seconds }"
+  def self.sum(time1, time2)
+    new.parse_and_calculate_total_units(time1, time2)
+    extra_minutes, seconds = @@total_seconds.divmod(60)
+    @@total_minutes += extra_minutes
+    extra_hours, minutes = @@total_minutes.divmod(60)
+    @@total_hours += extra_hours
+    day, hours = @@total_hours.divmod(24)
+    if day == 0
+      "The total time is #{ hours }:#{ minutes }:#{ seconds }"
+    else
+      "The total time is #{ day } day and #{ hours }:#{ minutes }:#{ seconds }"
+    end
   end
 
 end

1d112374d18158f0b135de656ebb39f1ad0ec7a6
Paresh Gupta <paresh@vinsol.com>
2014-07-22 14:00:03 +0530

    optimize code + make private methods

--- Ruby/Sum-Time(multiple)/lib/time.rb
+++ Ruby/Sum-Time(multiple)/lib/time.rb
@@ -4,38 +4,48 @@ require_relative '../lib/time_error.rb'
 class Time
   CHECK_TIME_REGEX = /(^([0-1]?\d|2[0-3]?)\:([0-5]?\d)\:([0-5]?\d)$)/
 
-  def valid?(time)
+  def self.sum(*time)
+    parse(time)
+    extra_minutes, @@seconds = @@total_seconds.divmod(60)
+    @@total_minutes += extra_minutes
+    extra_hours, @@minutes = @@total_minutes.divmod(60)
+    @@total_hours += extra_hours
+    @@day, @@hours = @@total_hours.divmod(24)
+    display
+  end
+
+  def self.valid?(time)
     CHECK_TIME_REGEX === time
   end
 
-  def parse_and_calculate_total_units(time)
+  def self.parse(time)
     @@total_hours, @@total_minutes, @@total_seconds = 0, 0, 0
     time.each do |time|
       if valid?(time)
         time = DateTime.parse(time)
-        @@total_hours += time.hour
-        @@total_minutes += time.minute
-        @@total_seconds += time.second
+        calculate_total_units(time)
       else
         raise TimeError, 'Invalid Time'
       end
     end
   end
 
-  def self.sum(*time)
-    new.parse_and_calculate_total_units(time)
-    extra_minutes, seconds = @@total_seconds.divmod(60)
-    @@total_minutes += extra_minutes
-    extra_hours, minutes = @@total_minutes.divmod(60)
-    @@total_hours += extra_hours
-    day, hours = @@total_hours.divmod(24)
-    if day == 0
-      "The total time is #{ hours }:#{ minutes }:#{ seconds }"
-    elsif day == 1
-      "The total time is #{ day } day and #{ hours }:#{ minutes }:#{ seconds }"
+  def self.calculate_total_units(time)
+    @@total_hours += time.hour
+    @@total_minutes += time.minute
+    @@total_seconds += time.second
+  end
+
+  def self.display
+    if @@day == 0
+      "The total time is #{ @@hours }:#{ @@minutes }:#{ @@seconds }"
+    elsif @@day == 1
+      "The total time is #{ @@day } day and #{ @@hours }:#{ @@minutes }:#{ @@seconds }"
     else
-      "The total time is #{ day } days and #{ hours }:#{ minutes }:#{ seconds }"
+      "The total time is #{ @@day } days and #{ @@hours }:#{ @@minutes }:#{ @@seconds }"
     end
   end
+
+  private_class_method :valid?, :parse, :calculate_total_units, :display
 
 end

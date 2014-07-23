b2f7f3b98eaa3ed163728513427ce8954875b004
Paresh Gupta <paresh@vinsol.com>
2014-07-23 13:00:51 +0530

    redefine initialize method with ternary operator

--- Ruby/Name-Raise/lib/name.rb
+++ Ruby/Name-Raise/lib/name.rb
@@ -4,30 +4,28 @@ class Name
   CHECK_LOWERCASE_ALPHABETS_OR_DIGITS_REGEX = /[a-z]|[\d]/
 
   def initialize(firstname, lastname)
-    @firstname = validate_length!(firstname) && check_first_letter_capital!(firstname)
-    @lastname = validate_length!(lastname)
+    @firstname = validate_length?(firstname) ? check_first_letter_capital!(firstname) : (raise InvalidNameError, 'First name is blank')
+    @lastname = validate_length?(lastname) ? lastname : (raise InvalidNameError, 'Last name is blank')
   end
-  
-  private
-    def validate_length!(name)
-      if name.nil? || name.empty?
-        raise NameError, "Name is Empty"
-      else
-        name
-      end
-    end
-  
-  private
-    def check_first_letter_capital!(name)
-      if name[0].match(CHECK_LOWERCASE_ALPHABETS_OR_DIGITS_REGEX)
-        raise NameError, 'First-letter of the name is not Capital'
-      else
-        name
-      end
-    end
 
   def to_s
     "#{ @firstname } #{ @lastname }"
   end
 
+  private
+    def validate_length?(name)
+      if name.nil? || name.empty?
+        false
+      else
+        true
+      end
+    end
+
+    def check_first_letter_capital!(name)
+      if name[0].match(CHECK_LOWERCASE_ALPHABETS_OR_DIGITS_REGEX)
+        raise InvalidNameError, "First letter of the '#{ name }' is not capital"
+      else
+        name
+      end
+    end
 end
